f37bdccdca1510c28d62c072de0c44f443908e28
sleepyshowdy <hojo.miku@gmail.com>
2023-02-25 19:07:37 +1100

    sb: ページコンポーネント追加

---
+++ stories/Page.tsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+// component---------
+import { Header } from "./Header";
+//type--------------
+import type { User } from "@/types";
+
+export const Page = () => {
+  const [user, setUser] = useState<User>();
+  return (
+    <article>
+      <Header
+        user={user}
+        onLogin={() => setUser({ name: "Meg" })}
+        onLogout={() => setUser(undefined)}
+        onCreateAccount={() => setUser({ name: "Meg" })}
+      />
+      <section>
+        <h2>◇◆タイトル◆◇</h2>
+      </section>
+    </article>
+  );
+};
