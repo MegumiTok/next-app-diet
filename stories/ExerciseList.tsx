6658709aa2b4766f92af8fb661cb4c4c529b8f9c
sleepyshowdy <hojo.miku@gmail.com>
2023-02-28 04:19:51 +1100

    feat: ✅

--- stories/ExerciseList.tsx
+++ stories/ExerciseList.tsx
@@ -2,8 +2,9 @@
 import DATA from "@/lib/local/exercise.json";
 import { Marker } from "./Marker";
 
-const today = "2021.05.21";
-const anotherDay = "2021.05.26";
+const today = "2022.02.28";
+const day_one = "2021.05.21";
+const day_two = "2021.05.26";
 
 interface IList {
   _day: string;
@@ -39,16 +40,22 @@ const List: React.FC<IList> = ({ _day }) => {
 };
 
 export const ExerciseList = () => {
+  const TODAY = DATA.filter((i) => i.date === today);
   return (
     <div className="my-10">
       <Marker text="My EXERCISE" />
       <div className="bg-white p-3 my-3" id="scroll">
-        {/* 今日 ------------------*/}
+        {TODAY.length === 0 && (
+          <div>
+            <div className="text-xl py-3">
+              <p>{today} (今日)</p>
+              <p>今日のエクササイズはまだありません</p>
+            </div>
+          </div>
+        )}
 
-        <List _day={today} />
-        {/* 違う日 ------------------*/}
-
-        <List _day={anotherDay} />
+        <List _day={day_one} />
+        <List _day={day_two} />
       </div>
     </div>
   );
